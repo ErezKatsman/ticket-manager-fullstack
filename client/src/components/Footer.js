259876b37a2f27a52cc69cacc208a405e308b3ef
ErezKatsman <erezkatsman2311@gmail.com>
2021-03-25 12:45:48 +0200

    adding a href to icons

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,10 +5,18 @@ export default function Footer() {
     <div className="footer">
       <span className="ek-logo">Erez Katsman</span>
       <div className="icons">
-        <i class="fa fa-github fa-2x"></i>
-        <i class="fa fa-linkedin fa-2x"></i>
-        <i class="fa fa-envelope fa-2x"></i>
-        <i class="fa fa-free-code-camp fa-2x"></i>
+        <a href="https://github.com/ErezKatsman">
+          <i class="fa fa-github fa-2x"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/erez-katsman-093704209/">
+          <i class="fa fa-linkedin fa-2x"></i>
+        </a>
+        <a href="mailto:someone@example.com">
+          <i class="fa fa-envelope fa-2x"></i>
+        </a>
+        <a href="https://www.freecodecamp.org/erezkatsman">
+          <i class="fa fa-free-code-camp fa-2x"></i>
+        </a>
       </div>
     </div>
   );
